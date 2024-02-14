612640d72ff87bc142632266ca0c9e2b358e2ecd
msmsm104 <84850535+msmsm104@users.noreply.github.com>
2024-02-14 17:52:41 +0900

    중복된 문자 제거

---
+++ codingTest/tutorial/delDuplication.py
@@ -0,0 +1,16 @@
+# delDuplication.py
+# 중복 문자 제거
+
+my_string = input()
+
+# print(set(list(my_string)))
+
+def delDuplication(data):
+    answer = ""
+
+    for x in data:
+        if x not in answer:
+            answer += x
+    return answer
+
+print(delDuplication(my_string))
