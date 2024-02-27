b99de057a8b87072af091ca1f8518faf82c9b35a
msmsm104 <msmsm104@naver.com>
2024-02-27 11:08:53 +0900

    minMaxSum 문제

---
+++ codingTest/hackerrank/minMaxSum.py
@@ -0,0 +1,28 @@
+# minMaxSum.py
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def minMaxSum(arr):
+
+    total_sum = sum(arr)
+    min_idx = 0
+    max_idx = 0
+    for i in range(len(arr)):
+        if arr[i] < arr[min_idx]:
+            min_idx = i
+        if arr[i] > arr[max_idx]:
+            max_idx = i
+    
+    print(total_sum - arr[max_idx], total_sum - arr[min_idx])
+
+
+if __name__=="__main__":
+    arr = list(map(int, input().rstrip().split()))
+
+    minMaxSum(arr)
+    
