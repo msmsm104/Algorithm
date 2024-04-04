2f235bb61bff11ed838e15c6d7b5aba6658001da
msmsm104 <84850535+msmsm104@users.noreply.github.com>
2024-04-04 18:19:09 +0900

    클래스 FourCal 생성

---
+++ algorithmNote/JumpToPython/Class/calculator.py
@@ -0,0 +1,38 @@
+# calculator.py
+# 사칙연산 클래스 생성
+
+class FourCal:
+    pass
+    # def __init__(self):
+    #     self.num1 = 0
+    #     self.num2 = 0
+
+    # def setdata(self, a, b):
+    #     self.num1 = a
+    #     self.num2 = b
+    
+    # def add(self):
+    #     result = self.num1 + self.num2
+    #     return result
+
+    # def sub(self):
+    #     result = self.num1 - self.num2
+    #     return result
+    
+    # def mul(self):
+    #     result = self.num1 * self.num2
+    #     return result
+    
+    # def div(self):
+    #     result = self.num1 / self.num2
+    #     return result
+    
+if __name__=="__main__":
+    a = FourCal() # 객체 a 생성, a는 FourCal 클래스의 인스턴스
+    print(type(a))
+    # a.setdata(4, 2)
+
+    # print(a.add())
+    # print(a.sub())
+    # print(a.mul())
+    # print(a.div())
