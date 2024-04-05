87b18083e39183aaa290db76a7797da709b3010d
msmsm104 <84850535+msmsm104@users.noreply.github.com>
2024-04-05 16:08:27 +0900

    변수, 클래스, 함수 생성

--- algorithmNote/JumpToPython/Module/mod2.py
+++ algorithmNote/JumpToPython/Module/mod2.py
@@ -1,3 +1,22 @@
 # mod2.py
 # 변수, 클래스, 함수를 포함하는 모듈
 
+PI = 3.141592
+
+
+class Math:
+    def solv(self, r):
+        result = PI * (r ** 2)
+        return result
+    
+
+def add(a, b):
+    result = a + b
+    return result
+
+if __name__=="__main__":
+    print(f"PI : {PI}")
+    print(f"add(4, 2) : {add(4, 2)}")
+
+    a = Math()
+    print(f"a.solv(5) : {a.solv(5)}")
