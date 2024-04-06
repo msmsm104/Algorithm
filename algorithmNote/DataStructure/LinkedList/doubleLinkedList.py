342e243b93a1738a6768285240602143f01c6a40
msmsm104 <84850535+msmsm104@users.noreply.github.com>
2024-04-06 18:52:59 +0900

    add_first 메서드 추가

--- algorithmNote/DataStructure/LinkedList/doubleLinkedList.py
+++ algorithmNote/DataStructure/LinkedList/doubleLinkedList.py
@@ -14,3 +14,30 @@ class DoubleLinkedList:
         self.tail.prev = self.head
         # 데이터 개수를 저장할 변수
         self.d_size = 0
+
+    # empty() 메서드 
+    def empty(self):
+        if self.d_size == 0:
+            return True
+        else:
+            return False
+        
+    # size() 메서드
+    def size(self):
+        return self.d_size
+    
+    # add_first() 메서드 
+    # new_node 의 prev, next 지정
+    # 기존 연결리스트의 head.next.prev, head.next 지정
+    def add_first(self, data):
+        new_node = node.Node(data) # 새로운 노드 생성
+
+        new_node.prev = self.head
+        new_node.next = self.head.next
+
+        self.head.next = new_node
+        self.head.next.prev = new_node
+
+        self.d_size += 1
+
+    
